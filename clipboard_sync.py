cb707eb9947886e2eea13e3d57264bb8503b1504
Igor Dudas <contact@igordudas.com>
2017-12-23 05:01:49 +0100

    version for stdin -> clipboard -> stdout

--- clipboard_sync.py
+++ clipboard_sync.py
@@ -2,9 +2,7 @@
 
 import tkinter as tk
 import sys
-from time import sleep
-import re
-from select import select
+import threading
 
 DELAY = 500
 sep = '/0'
@@ -12,22 +10,53 @@ escape_from = '/'
 escape_to   = '/ '
 
 root = tk.Tk()
-incoming = ''
 current = root.clipboard_get()
 print(current+sep, flush=True)
 
-def process_clipboard():
-    global incoming
-    global current
+new_content = None
+nc_lock = threading.Lock()
 
-    while sys.stdin in select([sys.stdin], [], [], 0) [0]:
-        incoming += sys.stdin.read(1)
-    split = incoming.split(sep)
-    incoming = split.pop()
-    if split:
-        tmp = split.pop()
+def read_input():
+    global new_content
+    incoming = ''
+    slash = False
+    while True:
+        next_char = sys.stdin.read(1)
+        if next_char == '':
+            break
+
+        if slash:
+            if next_char == '0':
+                # input complete
+                nc_lock.acquire()
+                new_content = incoming
+                nc_lock.release()
+                incoming = ''
+                slash = False
+            elif next_char == ' ':
+                incoming += '/'
+                slash = False
+            else:
+                incoming += '/' + next_char
+                slash = False
+        else:
+            if next_char == '/':
+                slash = True
+            else:
+                incoming += next_char
+
+
+
+def process_clipboard():
+    global current
+    global new_content
+
+    nc_lock.acquire()
+    if new_content is not None:
         root.clipboard_clear()
-        root.clipboard_append( tmp.replace(escape_to,escape_from) )
+        root.clipboard_append(new_content)
+        new_content = None
+    nc_lock.release()
 
     tmp = root.clipboard_get()
     if tmp != current:
@@ -36,6 +65,8 @@ def process_clipboard():
 
     root.after(DELAY, process_clipboard)
 
-root.after(DELAY, process_clipboard)
+th = threading.Thread(target=read_input)
+th.start()
 
+root.after(DELAY, process_clipboard)
 root.mainloop()
