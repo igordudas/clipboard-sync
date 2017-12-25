e0288b12d898453183e9a2188688684dafd6e198
Igor Dudas <contact@igordudas.com>
2017-12-25 03:31:59 +0100

    add communication through sockets

--- clipboard_sync.py
+++ clipboard_sync.py
@@ -3,24 +3,48 @@ import Tkinter as tk
 
 import sys
 import threading
+import socket
 
 DELAY = 500
-sep = u'/0'
-escape_from = u'/'
-escape_to   = u'/ '
+CLIENT_PORT = SERVER_PORT = 6666
 
-def write_str(s):
-    sys.stdout.write(s.encode('utf-8'))
-    sys.stdout.flush()
-
-root = tk.Tk()
-current = root.clipboard_get(type='UTF8_STRING')
-write_str(current.replace(escape_from,escape_to)+sep)
 
 new_content = None
 nc_lock = threading.Lock()
 
-def read_input():
+output_socket = None
+
+def read_input_socket(sock):
+    global new_content
+    length = 0
+    len_str = ''
+    incoming = ''
+
+    while True:
+        char = sock.recv(1)
+        # TODO: handle closing of connection
+        if 48 <= ord(char) <= 57:
+            len_str += char
+        elif char == ',':
+            length = int(len_str)
+            len_str = ''
+            incoming = sock.recv(length)
+            # TODO: handle closing of connection
+
+            nc_lock.acquire()
+            new_content = incoming.decode('utf-8', 'replace')
+            nc_lock.release()
+        else:
+            raise Exception('Length input contains illegal character: ' + char + '\n')
+
+
+def send_socket(unicode_msg):
+    msg = unicode_msg.encode('utf-8')
+    msg = str(len(msg)) + ',' + msg
+    output_socket.send(msg)
+
+
+def read_input_stdin():
     # hoffentlich gibt es hier keine komischen Kollisionen zwischen '/0' bzw.
     # '/ ' und einem Unicode-Zeichen
     global new_content
@@ -52,30 +76,68 @@ def read_input():
             else:
                 incoming += next_char
 
+def send_stdout(unicode_msg):
+    msg = (unicode_msg.replace('/', '/ ') + '/0') . encode('utf-8')
+    sys.stdout.write(msg)
+    sys.stdout.flush()
 
 
 def process_clipboard():
-    global current
     global new_content
-
-    nc_lock.acquire()
-    if new_content is not None:
-        root.clipboard_clear()
-        root.clipboard_append(new_content)
-        new_content = None
-    nc_lock.release()
-
-    tmp = root.clipboard_get(type='UTF8_STRING')
-    #sys.stderr.write('CLIPBOARD content type: ' + str(type(tmp)))
-    #sys.stderr.write((u'CLIPBOARD: '+tmp+u'\n').encode('utf-8'))
-    if tmp != current:
-        current = tmp
-        write_str(current.replace(escape_from,escape_to) + sep)
+    global current
 
     root.after(DELAY, process_clipboard)
 
-th = threading.Thread(target=read_input)
-th.start()
+    with nc_lock:
+        if new_content is not None:
+            root.clipboard_clear()
+            root.clipboard_append(new_content)
+            # do not send new content back over socket
+            current, new_content = new_content, None
+            return
+
+    tmp = root.clipboard_get(type='UTF8_STRING')
+    if tmp != current:
+        send(tmp)
+        current = tmp
+
+
+root = tk.Tk()
+root.withdraw()
+current = root.clipboard_get(type='UTF8_STRING')
+
+# stdin/stdout
+#th = threading.Thread(target=read_input_stdin)
+#th.start()
+#send = send_stdout
+#send(current)
+
+# socket
+send = send_socket
+if len(sys.argv) < 2:
+    sys.stderr.write('specify server/client as command line argument\n')
+    sys.exit()
+
+if sys.argv[1] == 'server':
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(('localhost', SERVER_PORT))
+    serversocket.listen(1)
+
+    clientsocket, adress = serversocket.accept()
+    th = threading.Thread(target=read_input_socket, args=(clientsocket,))
+    th.start()
+    output_socket = clientsocket
+elif sys.argv[1] == 'client':
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect(('localhost', CLIENT_PORT))
+    th = threading.Thread(target=read_input_socket, args=(clientsocket,))
+    th.start()
+    output_socket = clientsocket
+
+    send(current)
+else:
+    sys.stderr.write('specify server/client as command line argument\n')
+
 
 root.after(DELAY, process_clipboard)
 root.mainloop()
